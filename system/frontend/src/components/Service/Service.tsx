a0ee978f8315c03e37302e8a24bb6c56f82d53ae
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2024-10-22 13:59:14 +0200

    fix: added form component to service

--- system/frontend/src/components/Service/Service.tsx
+++ system/frontend/src/components/Service/Service.tsx
@@ -1,6 +1,7 @@
 import { FormEventHandler } from "react";
-import { InputField, Field, useFields } from "../Fields";
+import { useFields } from "../Fields";
 import { useClient } from "../../modules/Client";
+import {Form} from "../Form/Form";
 
 export function Service(_props: {}) {
     const fieldDefs = useFields()
@@ -24,66 +25,7 @@ export function Service(_props: {}) {
                 <h2 className="title is-size-4-mobile has-text-centered">Input</h2>
                 <div className="columns is-centered">
                     <div className="column is-two-thirds">
-                        <form onSubmit={handleSubmit}>
-                            <fieldset className="box">
-                                <legend className="label is-size-5 has-text-weight-medium has-text-grey-dark">Source sequences for target design</legend>
-                                <Field id="source_sequence.gene_transcript_name_or_fasta" widget={((props) => (<InputField {...props} type="textarea" rows={5}/>))}/>
-                                <div className="columns is-vcentered">
-                                    <div className="column">
-                                        <Field id="source_sequence.attribute_identifier" />
-                                    </div>
-                                    <div className="column">
-                                        <Field id="source_sequence.feature_identifier" />
-                                    </div>
-                                </div>
-                                <Field id="source_sequence.fasta_source_sequence_absent" />
-                            </fieldset>
-                            <fieldset className="box">
-                                <legend className="label is-size-5 has-text-weight-medium has-text-grey-dark">Probe design</legend>
-                                <div className="columns is-vcentered">
-                                    <div className="column">
-                                        <Field id="probe_design.probe_arm_length" />
-                                    </div>
-                                    <div className="column">
-                                        <Field id="probe_design.min_genome_distance" />
-                                    </div>
-                                </div>
-                                <Field id="probe_design.use_hamming_distance" />
-                                <Field id="probe_design.only_one_unique_arm" />
-                                <Field id="probe_design.allow_overlapping_probes" />
-                            </fieldset>
-                            <fieldset className="box">
-                                <legend className="label is-size-5 has-text-weight-medium has-text-grey-dark">Color code</legend>
-                                <div className="columns is-vcentered">
-                                    <div className="column">
-                                        <Field id="color_code.amount_of_colors" />
-                                    </div>
-                                    <div className="column">
-                                        <Field id="color_code.length_of_code" />
-                                    </div>
-                                </div>
-                            </fieldset>
-                            <fieldset className="box">
-                                <legend className="label is-size-5 has-text-weight-medium has-text-grey-dark">Anchor and spacer sequences</legend>
-                                <div className="columns is-vcentered">
-                                    <div className="column">
-                                        <Field id="anchor_and_spacer.anchor" widget={((props) => (<InputField {...props} type="textarea" rows={1}/>))}/>
-                                    </div>
-                                    <div className="column">
-                                        <Field id="anchor_and_spacer.spacer_left" widget={((props) => (<InputField {...props} type="textarea" rows={1}/>))}/>
-                                    </div>
-                                    <div className="column">
-                                        <Field id="anchor_and_spacer.spacer_right" widget={((props) => (<InputField {...props} type="textarea" rows={1}/>))}/>
-                                    </div>
-                                </div>
-                            </fieldset>
-                            <fieldset className="box">
-                                <legend className="label is-size-5 has-text-weight-medium has-text-grey-dark">Genome</legend>
-                                <Field id="genome.genome" />
-                            </fieldset>
-                            <button type="submit" className="button is-pulled-right has-background-grey has-text-white">Launch analysis</button>
-                            <button type="submit" className="button is-pulled-right has-background-grey has-text-white">Show example</button>
-                        </form>
+                        <Form handleSubmit={handleSubmit}/>
                     </div>
                 </div>
             </div>
