ffc14a6202ea39aa9f0c16481b1619a10732a88e
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2025-01-15 10:06:32 +0100

    feat: result view with styling, mock data, and no colors, issue #9

--- system/frontend/src/components/Result/Result.tsx
+++ system/frontend/src/components/Result/Result.tsx
@@ -8,6 +8,77 @@ export function Result(_props: {}) {
                 <h2 className="title is-size-4-mobile has-text-centered">{t("results.title")}</h2>
                 <div className="columns is-centered">
                     <div className="column is-two-thirds">
+                        <button type="submit" className="button is-light">Download tsv
+                        </button>
+                        <div className="table-container">
+                            <h2 className="title is-size-5-mobile is-size-4 mt-4 has-text-weight-normal">Padlock probes</h2>
+                            <table className="table is-striped">
+                                <thead>
+                                <tr>
+                                    <th>Gene</th>
+                                    <th>Symbol</th>
+                                    <th>Code</th>
+                                    <th>Position</th>
+                                    <th>UCSC</th>
+                                    <th>Strand</th>
+                                    <th>FeatureCoordinates</th>
+                                    <th>ProbeSeq</th>
+                                    <th>SpacerLeft</th>
+                                    <th>AnchorSeq</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>ENSDARG00000007196</td>
+                                        <td>rae1</td>
+                                        <td>322333</td>
+                                        <td>6:20267042	</td>
+                                        <td>Link</td>
+                                        <td>+</td>
+                                        <td>6:20267020-20267124</td>
+                                        <td>CCGATGATAGCATAAGCT-GCTTGGCATTTAGTCCTC</td>
+                                        <td>TCCTC</td>
+                                        <td>TGCGTCTATTTAGTGGAGCC</td>
+                                    </tr>
+                                    <tr>
+                                        <td>ENSDARG00000007196</td>
+                                        <td>rae1</td>
+                                        <td>322333</td>
+                                        <td>6:20267042	</td>
+                                        <td>Link</td>
+                                        <td>+</td>
+                                        <td>6:20267020-20267124</td>
+                                        <td>CCGATGATAGCATAAGCT-GCTTGGCATTTAGTCCTC</td>
+                                        <td>TCCTC</td>
+                                        <td>TGCGTCTATTTAGTGGAGCC</td>
+                                    </tr>
+                                    <tr>
+                                        <td>ENSDARG00000007196</td>
+                                        <td>rae1</td>
+                                        <td>322333</td>
+                                        <td>6:20267042	</td>
+                                        <td>Link</td>
+                                        <td>+</td>
+                                        <td>6:20267020-20267124</td>
+                                        <td>CCGATGATAGCATAAGCT-GCTTGGCATTTAGTCCTC</td>
+                                        <td>TCCTC</td>
+                                        <td>TGCGTCTATTTAGTGGAGCC</td>
+                                    </tr>
+                                    <tr>
+                                        <td>ENSDARG00000007196</td>
+                                        <td>rae1</td>
+                                        <td>322333</td>
+                                        <td>6:20267042	</td>
+                                        <td>Link</td>
+                                        <td>+</td>
+                                        <td>6:20267020-20267124</td>
+                                        <td>CCGATGATAGCATAAGCT-GCTTGGCATTTAGTCCTC</td>
+                                        <td>TCCTC</td>
+                                        <td>TGCGTCTATTTAGTGGAGCC</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
