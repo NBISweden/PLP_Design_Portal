10ce7e213f9340fbc6c5a79b6811f7f8f27117aa
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-10-01 13:32:15 +0200

    fix: add worker to start scripts

--- containers/scripts/start-script.sh
+++ containers/scripts/start-script.sh
@@ -11,4 +11,5 @@ install -d "$XDG_DATA_HOME"
 
 SCRIPTDIR=`dirname "$0"`
 caddy start --config "$SCRIPTDIR/Caddyfile"
+python -m backend.worker &
 exec gunicorn -w "${APP_WORKERS:-4}" "backend.app:create_app()" -b "0.0.0.0:${APP_PORT:-5000}"
